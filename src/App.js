ccfd5a3aff59cbe4e4804e568b5732adf38238ee
Julieta Lazzaro <julietalazzaro@gmail.com>
2020-09-10 07:48:23 -0300

    Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/auth/Login.js";
+import NuevaCuenta from "./components/auth/NuevaCuenta.js";
+import Proyectos from "./components/proyectos/Proyectos.js";
 function App() {
   return (
-    <div className="App">
-      <h1>MERN Tasks</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+        <Route exact path="/proyectos" component={Proyectos} />
+      </Switch>
+    </Router>
   );
 }
 
