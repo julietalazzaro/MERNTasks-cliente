0bc8faef509394be57444273c3b2ada131b3f77b
Julieta Lazzaro <julietalazzaro@gmail.com>
2020-09-12 09:24:45 -0300

    Integracion context en proyecto

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login.js";
 import NuevaCuenta from "./components/auth/NuevaCuenta.js";
 import Proyectos from "./components/proyectos/Proyectos.js";
+
+import ProyectoState from "./context/proyectos/proyectoState";
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-        <Route exact path="/proyectos" component={Proyectos} />
-      </Switch>
-    </Router>
+    <ProyectoState>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+          <Route exact path="/proyectos" component={Proyectos} />
+        </Switch>
+      </Router>
+    </ProyectoState>
   );
 }
 
